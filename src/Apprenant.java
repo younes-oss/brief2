a592af1ab48acad9243a09a06c99700d58fd3b0f
younes-oss <aitchattouyounes@gmail.com>
2024-12-18 17:13:41 +0100

    créer fonction ajouter

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Apprenant extends Personne{
 
     private Classe classe;
     private ArrayList<Double> notes;
+    private  ArrayList<Apprenant> apprenants=new ArrayList<>();
+
 
 
     public Apprenant(int id ,String nom, String prenom, String email) {
@@ -25,5 +28,20 @@ public class Apprenant extends Personne{
 
     public void setNotes(ArrayList<Double> notes) {
         this.notes = notes;
+    }
+
+    public void ajouter(){
+        int id=Personne.getId();
+        id=id++;
+        System.out.print("votre nom : \n");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.next();
+        System.out.print("votre prenom : \n");
+        String lastName = scanner.next();
+        System.out.print("votre email : \n");
+        String email = scanner.next();
+
+        apprenants.add(new Apprenant(id,firstName,lastName,email));
+
     }
 }
