5192f2c1259a658de656af54e7f6367821b11d4a
younes-oss <aitchattouyounes@gmail.com>
2024-12-18 16:37:05 +0100

    ajouter les accesseurs

--- src/Personne.java
+++ src/Personne.java
@@ -10,4 +10,44 @@ public abstract class Personne {
         this.email=email;
 
     }
+
+    public static int getCompteurId() {
+        return compteurId;
+    }
+
+    public static void setCompteurId(int compteurId) {
+        Personne.compteurId = compteurId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
