32c65e9315324b12bde9634271a353428b4a4490
younes-oss <aitchattouyounes@gmail.com>
2024-12-19 17:56:52 +0100

    ajouter la fonction modifier

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Apprenant extends Personne{
+     static Scanner scanner = new Scanner(System.in);
 
     private Classe classe;
     private ArrayList<Double> notes;
     private  ArrayList<Apprenant> apprenants=new ArrayList<>();
 
+    public Apprenant( String nom, String prenom, String email) {
+        super(nom, prenom, email);
 
-
-    public Apprenant(int id ,String nom, String prenom, String email) {
-        super(id,nom, prenom, email);
-
+    }
+    public Apprenant(){
     }
 
     public Classe getClasse() {
@@ -30,18 +31,53 @@ public class Apprenant extends Personne{
         this.notes = notes;
     }
 
-    public void ajouter(){
-        int id=Personne.getId();
-        id=id++;
-        System.out.print("votre nom : \n");
-        Scanner scanner = new Scanner(System.in);
-        String firstName = scanner.next();
-        System.out.print("votre prenom : \n");
-        String lastName = scanner.next();
-        System.out.print("votre email : \n");
-        String email = scanner.next();
+    public  void ajouterApprenant(){
 
-        apprenants.add(new Apprenant(id,firstName,lastName,email));
+        System.out.print("votre nom : ");
+        String firstName = scanner.next();
+        System.out.print("\n votre prenom : ");
+        String lastName = scanner.next();
+        System.out.print("\n votre email : ");
+        String email = scanner.next();
+        apprenants.add(new Apprenant(firstName,lastName,email));
 
     }
+    public void modiferApprenant(){
+        if(apprenants.isEmpty()){
+            System.out.print("la liste est vide");
+        }
+        System.out.print("entrer l'id de l'apprenant que vous voulez modifier");
+        int id =scanner.nextInt() ;
+
+        for (int i =0 ;i<apprenants.size();i++){
+            if(id ==apprenants.get(i).getId()){
+                System.out.print("votre nom : ");
+                String firstName = scanner.next();
+                System.out.print("\n votre prenom : ");
+                String lastName = scanner.next();
+                System.out.print("\n votre email : ");
+                String email = scanner.next();
+                //Apprenant apprenant =new Apprenant();
+//                apprenant.setPrenom(firstName);
+//                apprenant.setNom(lastName);
+//                apprenant.setEmail(email);
+                apprenants.get(i).setNom(firstName);
+                apprenants.get(i).setPrenom(lastName);
+                apprenants.get(i).setEmail(email);
+            }
+        }
+
+    }
+
+    public void AfficherApprenant(){
+
+        for(int i =0 ; i<apprenants.size();i++){
+
+            System.out.println("id : " +apprenants.get(i).getId()+" full name : "+ apprenants.get(i).getNom() +" "+ apprenants.get(i).getPrenom()+" email: " + apprenants.get(i).getEmail());
+
+        }
+    }
+
+
+
 }
