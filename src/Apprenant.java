e524e4c8194137322d28ea50af396addc5f4a0d4
younes-oss <aitchattouyounes@gmail.com>
2024-12-20 15:16:11 +0100

    ajouter la fonction supprimer

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -78,6 +78,28 @@ public class Apprenant extends Personne{
         }
     }
 
+    public void supprimerApprenant(){
 
+        if(apprenants.isEmpty()){
+            System.out.print("la liste est vide");
+            return;
+        }
+        System.out.print("entrer l'id de l'apprenant que vous voulez supprimer");
+        int choix;
+        choix=scanner.nextInt();
+        int apprenantId = 0;
+        boolean trouve = false;
+        for(int i = 0; i<apprenants.size();i++){
+            if(choix == apprenants.get(i).getId()){
+            apprenantId=  apprenants.indexOf(apprenants.get(i) );
+            apprenants.remove(apprenantId);
+            System.out.println("supprimée avec succées");
+            trouve=true;
+            }
+        }
+        if(!trouve) {
+            System.out.println("ce apprenant n'est existe pas\n");
+        }
+    }
 
 }
