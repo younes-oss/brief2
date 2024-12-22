25ef2c1872a51d62a0b92e4262fcae7fad7cb838
younes-oss <aitchattouyounes@gmail.com>
2024-12-22 19:37:52 +0100

    fonction ajouterFormateur

--- src/Application.java
+++ src/Application.java
@@ -11,17 +11,19 @@ public class Application {
         int choix = 0;
         Scanner scanner = new Scanner(System.in);
 
-        while (choix !=4){
+        while (choix != 4) {
 
             System.out.println("1.\" Gestion de Systeme Apprenant \"");
             System.out.println("2.\" Gestion de Systeme Fomrmateur \"");
             System.out.println("3.\" Gestion de Systeme Classe \"");
             System.out.println("4.\" Quitter \"");
             choix = scanner.nextInt();
-            switch (choix){
-                case 1 : menuApprenant();
+            switch (choix) {
+                case 1:
+                    menuApprenant();
                     break;
                 case 2:
+                    menuFormateur();
                     break;
                 case 3:
                     break;
@@ -29,28 +31,25 @@ public class Application {
                     System.exit(0);
                     break;
             }
-
-
         }
-
-
     }
 
-    private static void menuApprenant(){
+    public static void menuApprenant() {
         int choix = 0;
         Apprenant apprenant = new Apprenant();
         Scanner scanner = new Scanner(System.in);
-        while (choix !=6){
+        while (choix != 6) {
             System.out.println("   ***********************************************");
             System.out.println("   *  1.\" Ajouter un Apprenant a une classe \"  *");
             System.out.println("   *  2.\"Associer un Apprenant a une classe \"  *");
             System.out.println("   *  3.\" modifier un Apprenant \"              *");
             System.out.println("   *  4.\" afficher un Apprenant \"              *");
-            System.out.println("   *  5.\" supprimer un Apprenant \"              *");
+            System.out.println("   *  5.\" supprimer un Apprenant \"             *\n" +
+                    "   *  6.\" retourner a la liste principale\"     *");
 
             choix = scanner.nextInt();
-            switch (choix){
-                case 1 :
+            switch (choix) {
+                case 1:
                     apprenant.ajouterApprenant();
                     break;
                 case 2:
@@ -58,18 +57,54 @@ public class Application {
                 case 3:
                     apprenant.modiferApprenant();
                     break;
-                case 4 :
+                case 4:
                     apprenant.AfficherApprenant();
                     break;
                 case 5:
                     apprenant.supprimerApprenant();
                     break;
                 case 6:
-                    System.exit(0);
+                    System.out.println("\" voila la liste principale \" \n");
                     break;
             }
+        }
+    }
+
+    public static void menuFormateur() {
+        Formateur formateur = new Formateur();
+        int choix = 0;
+        System.out.print("-----------------Gestion des Formateurs-----------------\n" +
+                "     |\"    1.ajouter un formateur a une classe \"\n         |" +
+                "     |\"    2.associer un formateur a une classe \"\n        |" +
+                "     |\"    3.modifier un formateur par une classse \"\n     |" +
+                "     |\"    4.afficher tous les formateurs\"\n               |" +
+                "     |\"    5.supprimer un formateur\"\n                     |" +
+                "     |\"    6.retourner a la liste principale                |");
+        Scanner scanner = new Scanner(System.in);
+        choix = scanner.nextInt();
+        while (choix != 6) {
+            switch (choix) {
+                case 1:
+                    formateur.ajouterFormrateur();
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
 
 
+                    break;
+                case 6:
+                    System.out.println("\" voila la liste principale \" \n");
+                    break;
+
+
+            }
         }
     }
 }
--- src/Formateur.java
+++ src/Formateur.java
@@ -1,18 +1,25 @@
 package brief2.src;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Formateur extends Personne {
     private String specialite;
     private double salaire;
     private Classe classe;
+    private ArrayList<Formateur> formateurs =new ArrayList<>();
 
 
 
-    public Formateur( String nom, String prenom, String email,String specialite,double salaire,Classe classe) {
+    public Formateur( String nom, String prenom, String email,String specialite,double salaire) {
         super(nom, prenom, email);
         this.specialite=specialite;
         this.salaire=salaire;
         this.classe=classe;
     }
+    public Formateur(){}
+
 
     public String getSpecialite() {
         return specialite;
@@ -37,4 +44,23 @@ public class Formateur extends Personne {
     public void setClasse(Classe classe) {
         this.classe = classe;
     }
+    public void ajouterFormrateur(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("votre nom : ");
+        String firstName = scanner.next();
+        System.out.print("\n votre prenom : ");
+        String lastName = scanner.next();
+        System.out.print("\n votre email : ");
+        String email = scanner.next();
+        System.out.print("\n votre specialite : ");
+        String specialite = scanner.next();
+        System.out.print("\n votre salaire : ");
+        double Salaire = scanner.nextDouble();
+
+        formateurs.add(new Formateur(firstName,lastName,email,specialite,Salaire));
+    }
+
+    
+
+
 }
